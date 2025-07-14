1d42e34deb2b8f32e8d89770c4665e25a6a57b03
mohanataraj <mohan1519@gmail.com>
2025-07-14 06:08:55 -0700

    Initial Commit

---
+++ GISToolsKit/GISToolsKit.swift
@@ -0,0 +1,9 @@
+//
+//  GISToolsKit.swift
+//  GISToolsKit
+//
+//  Created by Mohan Nataraj on 7/14/25.
+//
+
+import Foundation
+
---
+++ GISToolsKitTests/GISToolsKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  GISToolsKitTests.swift
+//  GISToolsKitTests
+//
+//  Created by Mohan Nataraj on 7/14/25.
+//
+
+import Testing
+@testable import GISToolsKit
+
+struct GISToolsKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
